d6c461164d3d604f5f0c5e6e2fd0cf53df83844b
ramadhanrenaldy <ramadhanrenaldy@upgris.ac.id>
2024-12-12 14:47:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Proyek Pertama Machine Learning')
 
 st.info('App ini digunakan untuk melakukan deployment Machine Learning')
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
+df
